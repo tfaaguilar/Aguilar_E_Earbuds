ae6089458910d42c61340f76ce4d06dfb1179517
tfaaguilar@gmail.com <e_aguilarvelez@fanshaweonline.ca>
2023-11-02 14:21:29 -0400

    Updating js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// Logo animation using "Green Sock"
+
+const logo = document.getElementById('logo');
+
+    gsap.to(logo, {
+        duration: 1, 
+        x: 20, 
+        rotation: 360, 
+        scale: 2, 
+        backgroundColor: "#F2D8D8", 
+        ease: "power2.inOut", 
+    });
+
+
+//Burger menu
+
+(function(){
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burger-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
+
+	button.addEventListener("click", hamburgerMenu, false);		
+})();
+
+//Earbuds Model
 (() => {
   console.log("IIFE Fired");
 
